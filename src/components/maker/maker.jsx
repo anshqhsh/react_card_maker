e8d36c0cf1bc91600eff5e3b016c9da2b95d4e45
JOON HYUK LEE <joonhyuklee@MacBook-Pro-2.local>
2020-11-22 13:37:17 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
@@ -9,6 +9,41 @@ import styles from './maker.module.css';
 
 //props 에 onLogout을 전달 , authService의 logout을 이용
 const Maker = ({ authService }) => {
+        const [cards, setCards] = useState([
+            {
+                id: '1',
+                name: 'LEEJH1',
+                company: 'kornec',
+                theme: 'dark',
+                title: 'Software Enfineer',
+                email: 'anshqhsh.dev@gmail.com',
+                message: 'contect me',
+                fileName: 'Joon',
+                fileURL: null,
+            },
+            {
+                id: '2',
+                name: 'LEEJH2',
+                company: 'kornec',
+                theme: 'light',
+                title: 'Software Enfineer',
+                email: 'anshqhsh.dev@gmail.com',
+                message: 'contect me',
+                fileName: 'Joon',
+                fileURL: null,
+            },
+            {
+                id: '3',
+                name: 'LEEJH3',
+                company: 'kornec',
+                theme: 'colorful',
+                title: 'Software Enfineer',
+                email: 'anshqhsh.dev@gmail.com',
+                message: 'contect me',
+                fileName: 'Joon',
+                fileURL: null,
+            }
+        ]); // useState : 상태값 관리 state의 값과 state 값 세팅하는 함수 를 리턴
         const history = useHistory();
         const onLogout = () => {
             authService.logout();
@@ -25,8 +60,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
                 <div className={styles.container}>
-                    <Editor />
-                    <Preview />
+                    <Editor cards ={cards}/>
+                    <Preview cards ={cards}/>
                 </div>
             <Footer />
         </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Card from '../card/card';
 import styles from './preview.module.css'
 
-const Preview = props => (
+const Preview = ({cards}) => (
     <section className={styles.preview}>
         <h1 className={styles.title}>Card Preview</h1>
+        <ul className={styles.cards}>
+             {cards.map(card =>( //카드로 받아온 값을 원하는 컴포넌트로 생성 
+                <Card card={card} /> //Card라는 props안에 card object를 전달
+            ))}
+        </ul>
     </section>
 );        
 export default Preview;
