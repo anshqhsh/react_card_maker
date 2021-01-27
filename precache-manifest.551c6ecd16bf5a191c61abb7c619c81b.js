3fc081f2e018836770c833f7856ecc453c7c1562
LJH's MacBookPro16 <joonhyuklee@MacBook-Pro-2.local>
2021-01-28 01:47:00 +0900

    Updates

---
+++ precache-manifest.551c6ecd16bf5a191c61abb7c619c81b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc861833cf2843a6960c2d8750a889",
+    "url": "/react_card_maker/index.html"
+  },
+  {
+    "revision": "f75f0af090cdfd6411df",
+    "url": "/react_card_maker/static/css/main.6bc8c7da.chunk.css"
+  },
+  {
+    "revision": "0a4058ecd11f5d2d545b",
+    "url": "/react_card_maker/static/js/2.5b5bbb4e.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/react_card_maker/static/js/2.5b5bbb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75f0af090cdfd6411df",
+    "url": "/react_card_maker/static/js/main.d26d335c.chunk.js"
+  },
+  {
+    "revision": "e9ff3abe27c01ff3b88b",
+    "url": "/react_card_maker/static/js/runtime-main.7fde8178.js"
+  }
+]);
